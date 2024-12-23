4b4cefe55eccc3b998648b28e749cae54cdc7630
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-12-24 01:07:50 +0530

    added new code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("hello world from me");
 
-console.log("wea are gonna solve merge conflict in github");console.log("hello world from feature 1");
+console.log("let us solve advance merge conflict");
 
 console.log("solve the merge confilct in this code");
+console.log("is it a end of the code");
