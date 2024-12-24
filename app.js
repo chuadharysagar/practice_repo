05c4d348c1cc81fa5068564b076f6f2f7bf174b0
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-12-25 00:04:05 +0530

    resolve conflict

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ console.log("hello world from me");
 console.log("let us solve advance merge conflict");
 
 console.log("solve the merge confilct in this code");
+console.log("hellu new chnage from feature 1");
 console.log("is it a end of the code");
